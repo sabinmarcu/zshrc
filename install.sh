d103c11ed8fc4ad0443102257f14df0bb81f5483
Sabin Marcu <sabinmarcu@gmail.com>
2016-07-16 11:02:30 +0300

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+echo "=== Installing Oh-My-ZSH"
+if [ $(command -v curl) ]; then 
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+else
+  sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+fi
+
+echo "=== Installing local config"
+cd ~ZSH_CUSTOM
+mv plugins plugins.old
+if [ $(command -v git) ]; then
+  git clone http://github.com/sabinmarcu/zshrc.git plugins
+else 
+  if [ $(command -v curl) ]; then 
+    sh -c "$(curl -OL http://github.com/sabinmarcu/zshrc/archive/master.zip)"
+  else
+    sh -c "$(wget http://github.com/sabinmarcu/zshrc/archive/master.zip)"
+  fi
+  unzip master.zip
+  mv zshrc-master plugins
+  rm master.zip
+fi
+cd plugins
+ln -s ~/.zshrc ~ZSH_CUSTOM/plugins/zshrc
+
+echo "=== Done"
